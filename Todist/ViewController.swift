d802a0db542d46fe9710529350ebca15083af681
Patrick Devine <patrick@patricks-mbp-2.fios-router.home>
2016-07-17 16:40:49 -0400

    Initial Commit

---
+++ Todist/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Todist
+//
+//  Created by Patrick Devine on 7/17/16.
+//  Copyright © 2016 interview.assignment. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
